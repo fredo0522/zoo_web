5d7b04564b3499a6c31db24e3747c5c4c6a21a60
Fredo <luisalfredo9905@gmail.com>
2020-10-20 22:30:02 -0500

    agregando search input a la pagina, tanto al navbar como a la pagina de animales

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -18,31 +18,40 @@ class HeaderBar extends Component {
           </NavLink>
         </div>
 
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <div className="nav-link">
-              <NavLink
-                activeClassName='nav-item active'
-                className='nav-link'
-                to="/animals" exact
-              >
-                Animales
-              </NavLink>
+        <div className="navbar-collapse collapse show" id="navbarCollapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <div className="nav-link">
+                <NavLink
+                  activeClassName='nav-item active'
+                  className='nav-link'
+                  to="/animals" exact
+                >
+                  Animales
+                </NavLink>
+              </div>
+            </li>
+            <li className="nav-item mr-auto">
+              <div className="nav-link">
+                <NavLink
+                  activeClassName='nav-item active'
+                  className='nav-link'
+                  to='/who'
+                  exact
+                >
+                  ¿Quienes somos?
+                </NavLink>
+              </div>
+            </li>
+          </ul>
+
+          <form className="form-inline mt-2 mt-md-0">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon3">Search</span>
             </div>
-          </li>
-          <li className="nav-item">
-            <div className="nav-link">
-              <NavLink
-                activeClassName='nav-item active'
-                className='nav-link'
-                to='/who'
-                exact
-              >
-                ¿Quienes somos?
-              </NavLink>
-            </div>
-          </li>
-        </ul>
+            <input className="form-control mr-sm-2" type="text" aria-describedby="basic-addon3"/>
+          </form>
+        </div>
       </nav>
     );
   }
--- src/pages/Animals.jsx
+++ src/pages/Animals.jsx
@@ -7,6 +7,12 @@ class Animals extends Component {
   render() {
     return(
       <div className='container mt-4'>
+        <div className="input-group mb-3">
+          <div class="input-group-prepend">
+            <span className="input-group-text" id="basic-addon3">¿Qué animal estas buscando?</span>
+          </div>
+          <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3"/>
+        </div>
         <div className='row row-cols-md-3'>
           {
             this.props.animals.map(animal => {
